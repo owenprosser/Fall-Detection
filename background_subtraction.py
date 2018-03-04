fd516e230e4136b1273ab4ec3b0a55dd16066342
Owen Prosser <owenjprosser@gmail.com>
2018-03-04 15:45:28 +0000

    Background Subtraction improved

--- background_subtraction.py
+++ background_subtraction.py
@@ -1,23 +1,27 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('test2.avi')
 fgbg = cv2.createBackgroundSubtractorMOG2()
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+kernel = np.ones((3,3),np.uint8)
 
 while(1):
     ret, frame = cap.read()
 
     fgmask = fgbg.apply(frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-    cv2.imshow('fgmask',frame)
-    cv2.imshow('frame',fgmask)
 
-    
+    _, bgrThresh = cv2.threshold(fgmask,254,255,cv2.THRESH_BINARY)
+
+    #bgrThresh = cv2.erode(bgrThresh,kernel,iterations = 1)
+    bgrThresh = cv2.morphologyEx(bgrThresh, cv2.MORPH_OPEN, kernel)
+    bgrThresh = cv2.morphologyEx(bgrThresh, cv2.MORPH_CLOSE, kernel)
+
+    #cv2.imshow('fgmask',frame)
+    cv2.imshow('Background Removed',bgrThresh)
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
     
-
 cap.release()
 cv2.destroyAllWindows()
