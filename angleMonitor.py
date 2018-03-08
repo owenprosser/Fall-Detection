21e1c9f13129393ee2b3b28a9d5c043778003e82
Owen Prosser <owenjprosser@gmail.com>
2018-03-08 20:03:51 +0000

    Simplified screen text concatenation

--- angleMonitor.py
+++ angleMonitor.py
@@ -8,18 +8,19 @@ class angleMonitor:
         print("angleMonitor",curFrame)
 
         _, contours, hierarchy = cv2.findContours(frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        maxContour = max(contours, key = cv2.contourArea)
         cnt = contours[0]
         moments = cv2.moments(cnt)
         area = moments['m00']
 
         frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2RGB)
 
-        if len(cnt) > 4:
-            maxContour = max(contours, key = cv2.contourArea)
+        if len(maxContour) > 4:
             (x,y),(MA,ma),angle = cv2.fitEllipse(maxContour)
             ellipse = cv2.fitEllipse(maxContour)
             cv2.ellipse(frame,ellipse,(0,255,0),2)
-            cv2.putText(frame,'Angle: '+str(angle) ,(5,25), self.font, 2,(242, 238, 26),2,cv2.LINE_AA)
+            screenText = 'Angle: '+str(angle)+' Area: '+str(area)
+            cv2.putText(frame,screenText ,(5,25), self.font, 2,(242, 238, 26),2,cv2.LINE_AA)
         else:
             print("Less than 5 points in contour array")
             cv2.putText(frame,'Less than 5 points in contour array',(5,25), self.font, 2,(242, 238, 26),2,cv2.LINE_AA)
