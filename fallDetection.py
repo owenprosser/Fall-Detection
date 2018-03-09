2e3c7f0d5d7ea88cafa3f1c0df76c94c44499482
Owen Prosser <owenjprosser@gmail.com>
2018-03-09 14:10:49 +0000

    Changed moph operators

--- fallDetection.py
+++ fallDetection.py
@@ -13,25 +13,26 @@ class backgroundSub:
         fgbg = cv2.createBackgroundSubtractorMOG2(self.history, self.varThresh ,self.detectShadows)
 
         while(cap != None):
-            self.curFrame += 1
-            ret, frame = cap.read()
+            if (self.curFrame % 1 == 0):
+                ret, frame = cap.read()
 
-            fgmask = fgbg.apply(frame,0)
-            bgrThresh = fgmask
-            _, bgrThresh = cv2.threshold(fgmask,250,255,cv2.THRESH_BINARY)
-            
-            bgrThresh = cv2.erode(bgrThresh,self.kernel, iterations = 1)
-            bgrThresh = cv2.morphologyEx(bgrThresh, cv2.MORPH_CLOSE, self.kernel)
-            bgrThresh = cv2.morphologyEx(bgrThresh, cv2.MORPH_OPEN, self.kernel)
-            bgrThresh = cv2.dilate(bgrThresh,self.kernel, iterations = 1)
+                fgmask = fgbg.apply(frame,0)
+                bgrThresh = fgmask
+                _, bgrThresh = cv2.threshold(fgmask,250,255,cv2.THRESH_BINARY)
+                
+                bgrThresh = cv2.erode(bgrThresh,self.kernel, iterations = 2)
+                bgrThresh = cv2.morphologyEx(bgrThresh, cv2.MORPH_CLOSE, self.kernel)
+                bgrThresh = cv2.morphologyEx(bgrThresh, cv2.MORPH_OPEN, self.kernel)
+                bgrThresh = cv2.dilate(bgrThresh,self.kernel, iterations = 1)
 
-            if (self.curFrame % 1 == 0) & cv2.countNonZero(bgrThresh) > 0:
-                det.Detect(bgrThresh, self.curFrame)
-            fall.check()
+                if cv2.countNonZero(bgrThresh) > 0:
+                    det.Detect(bgrThresh, self.curFrame)
+                fall.check()
 
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
-                break
+                self.curFrame += 1
+                k = cv2.waitKey(30) & 0xff
+                if k == 27:
+                    break
 
         cap.release()
         cv2.destroyAllWindows()
